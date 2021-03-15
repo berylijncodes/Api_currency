9c63c23576dc6b7c2ff56f2ce23ea8dabf163d89
berylijncodes <berylijn@gmail.com>
2021-03-15 10:19:57 -0700

    edit welcome message

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/api/rates', async (req, res) =>
 
 app.get('/', (req, res) =>
 {
-    return res.send('Welcome Home')
+    return res.send('Welcome to Currency Home')
    // console.log(response);
 });
 
